50eabc917be60a4989c2dcc59c1fe09d8758e63a
Brad Milne <brad.milne@getelevate.com>
2012-12-18 17:06:41 -0500

    edit answer form in activeadmin

--- app/admin/answers.rb
+++ app/admin/answers.rb
@@ -1,8 +1,12 @@
 ActiveAdmin.register Answer do
 
+
+  # form to add answers to questions
+  # the questions are currently shown as their explanation 
   form do |f|
     f.inputs "Answer" do
-      f.input :question_id, :as => :select, :collection => Question.all.map(&:explanation)
+      # the line below shows question_id as the question.explanation
+      f.input :question_id, :as => :select, :collection => Question.all.map{ |question| [question.explanation, question.id]}
       f.input :answer
     end
     
