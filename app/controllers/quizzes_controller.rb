dda09d59cdc89a9d0d791bb067e374660e89f187
Brad Milne <brad.milne@getelevate.com>
2013-02-17 14:44:08 -0500

    add in shuffled questions to quiz page

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,6 +3,9 @@ class QuizzesController < ApplicationController
   def new
     @lesson = Lesson.find(params[:lesson_id])
     @questions = @lesson.questions.all
+    @question1 = @questions.shuffle[1]
+    @question2 = @questions.shuffle[1]
+    @question3 = @questions.shuffle[1]
   end
   
   def show
