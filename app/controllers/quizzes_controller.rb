f1892987c54e9a06c52e57a4509c7da383298a9d
Brad Milne <brad.milne@getelevate.com>
2013-02-20 20:24:36 -0500

    modify quiz controller to calculate each question value and total score.

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -6,25 +6,35 @@ class QuizzesController < ApplicationController
     @question1 = @questions.shuffle[1]
     @question2 = @questions.shuffle[1]
     @question3 = @questions.shuffle[1]
+    @question4 = @questions.shuffle[1]
+    @question5 = @questions.shuffle[1]
   end
   
   
   
   def create
-    @score = 0
-    if params[:user_answer1] == params[:correct_answer1]
-      @score = @score + 1
-      flash[:notice] = "Yes, you got it right!"
-      render 'show' 
+    @question1_score = params[:user_answer1] == params[:correct_answer1] ? 1 : 0
+    @question2_score = params[:user_answer2] == params[:correct_answer2] ? 1 : 0
+    @question3_score = params[:user_answer3] == params[:correct_answer3] ? 1 : 0
+    @score = @question1_score + @question2_score + @question3_score
+    if @score == 3 
+      flash[:notice] = "Unbelievable! You got #{@score} out of 5!"
+      render 'show'
+    elsif @score == 2
+      flash[:notice] = "Good work! You got #{@score} out of 5!"
+      render 'show'    
+    elsif @score == 1
+      flash[:notice] = "Hmmm.. you might need some more practice. You got #{@score} out of 5."
+      render 'show'
     else
-      flash[:notice] = "Sorry you got it wrong"
-      @score = @score
+      flash[:notice] = "Keep at it and you'll start to get it! You got #{@score} out of 5."
       render 'show'
     end
   end
   
   def show
     @score
+    @question1_score
   end
   
 end
