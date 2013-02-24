066e70fd0ec27c00979c011df2310a4ba173dc9d
Brad Milne <brad.milne@getelevate.com>
2013-02-24 09:51:16 -0500

    add callback before_create to convert score to percentage

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,5 +2,9 @@ class Quiz < ActiveRecord::Base
   attr_accessible :score, :lesson_id
   belongs_to :lesson, :polymorphic => true
   accepts_nested_attributes_for :lesson
- 
+  before_create :calculate_score
+  
+  def calculate_score
+    self.score = self.score * 20
+  end
 end
